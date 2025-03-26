34bb0d727cd3712ecc7c30eb3b47bb8693030b62
Eduardo Ávila <ea618369@gmail.com>
2025-03-26 11:51:59 -0300

    final

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const lamp =  document.getElementById('lamp')
 const btnTurnOn = document.getElementById('turnOn')
 const btnTurnOff = document.getElementById('turnOff')
+const imagem = document.getElementById('imagem')
 
 btnTurnOn.addEventListener("click", () => {
     if( lamp.className == "off"){
         lamp.classList.remove('off')
         lamp.classList.add ('on')
     }
+    imagem.src = "./img/ligada.png"
     console.log(lamp.className)
 })
 
@@ -15,5 +17,6 @@ btnTurnOff.addEventListener("click", () => {
         lamp.classList.remove('on')
         lamp.classList.add ('off')
     }
+    imagem.src = "./img/desligada.png"
     console.log(lamp.className)
 })
